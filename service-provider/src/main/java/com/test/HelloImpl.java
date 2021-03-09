781c2165192c050cf1000246f85b5f1760357fc6
thbspan <thbspan@163.com>
2021-03-09 17:30:22 +0800

    add exception filter example

--- service-provider/src/main/java/com/test/HelloImpl.java
+++ service-provider/src/main/java/com/test/HelloImpl.java
@@ -10,6 +10,11 @@ public class HelloImpl implements IHello {
 
     @Override
     public TestBean get(int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException("id must >= 0");
+        } else if (id >= 10000) {
+            throw new RuntimeException("id is too big!");
+        }
         TestBean testBean = new TestBean();
         testBean.setName("test");
         return testBean;
